f0ae8038b92de8e16c1ee24bbec0f1c19c5d1273
Li Yuzhuo <lee.yuzhuo233@gmail.com>
2022-06-16 23:29:23 +0800

    调整目录结构2

--- utils.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
-
-
-def normalize(_max, _min, score):
-    res = 1 - (score - _min) / (_max - _min)
-    return res
